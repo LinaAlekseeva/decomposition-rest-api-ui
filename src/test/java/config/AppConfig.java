e8a20de88696ac8a5f81fc9bcb8084c39b1ea40b
Lina Alekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-08-03 15:33:42 +0300

    Update AppConfig.java

--- src/test/java/config/AppConfig.java
+++ src/test/java/config/AppConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:resources/app.properties"
+        "classpath:app.properties"
 })
 public interface AppConfig extends Config {
 
